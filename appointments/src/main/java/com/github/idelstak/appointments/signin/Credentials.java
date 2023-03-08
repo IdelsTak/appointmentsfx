3498c8fc5550c332653f2ddde8e2d376f7629fe7
Hiram K <hiram.kamau@outlook.com>
2023-03-08 17:52:46 +0300

    Update Credentials.java

--- appointments/src/main/java/com/github/idelstak/appointments/signin/Credentials.java
+++ appointments/src/main/java/com/github/idelstak/appointments/signin/Credentials.java
@@ -1,6 +1,30 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 Hiram K
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.github.idelstak.appointments.signin;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Credentials {
 
@@ -22,6 +46,32 @@ public class Credentials {
 
     private static char[] copyOf(char[] original) {
         return Arrays.copyOf(original, original.length);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 31 * hash + Objects.hashCode(this.username);
+        hash = 31 * hash + Arrays.hashCode(this.password);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Credentials other = (Credentials) obj;
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+        return Arrays.equals(this.password, other.password);
     }
 
 }
