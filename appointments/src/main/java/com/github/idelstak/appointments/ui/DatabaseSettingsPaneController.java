4cdae8597f3d2fa65abc40738d37ec4f0d7fafe7
Hiram K <hiram.kamau@outlook.com>
2023-03-08 10:33:19 +0300

    Create DatabaseSettingsPaneController.java

---
+++ appointments/src/main/java/com/github/idelstak/appointments/ui/DatabaseSettingsPaneController.java
@@ -0,0 +1,81 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 Hiram K
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.github.idelstak.appointments.ui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class DatabaseSettingsPaneController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ProgressIndicator connectionAttemptProgress;
+
+    @FXML
+    private Label connectionErrorStatusLabel;
+
+    @FXML
+    private Label connectionStatusLabel;
+
+    @FXML
+    private TextField databaseNameTextField;
+
+    @FXML
+    private VBox databaseSettingsPane;
+
+    @FXML
+    private TextField databaseURLTextField;
+
+    @FXML
+    private TextField hostTextField;
+
+    @FXML
+    private PasswordField passwordTextField;
+
+    @FXML
+    private Button saveConnectionSettingsButton;
+
+    @FXML
+    private Button testConnectionButton;
+
+    @FXML
+    private TextField userNameTextField;
+
+    @FXML
+    protected void initialize() {
+
+    }
+}
