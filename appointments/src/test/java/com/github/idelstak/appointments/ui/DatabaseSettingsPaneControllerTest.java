8d8407c60783460dce6af15bb3f85f75a8b560a8
Hiram K <hiram.kamau@outlook.com>
2023-03-08 13:30:39 +0300

    Update DatabaseSettingsPaneControllerTest.java

--- appointments/src/test/java/com/github/idelstak/appointments/ui/DatabaseSettingsPaneControllerTest.java
+++ appointments/src/test/java/com/github/idelstak/appointments/ui/DatabaseSettingsPaneControllerTest.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 Hiram K
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.github.idelstak.appointments.ui;
 
 import com.github.idelstak.appointments.database.DatabaseConnectionPreferences;
@@ -6,14 +29,20 @@ import com.github.idelstak.appointments.database.DisplayedView;
 import java.net.URI;
 import java.util.prefs.Preferences;
 import javafx.scene.Parent;
+import javafx.scene.control.Control;
+import javafx.scene.control.Labeled;
 import javafx.scene.control.TextInputControl;
 import javafx.scene.input.MouseButton;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.Test;
+import org.testfx.assertions.api.Assertions;
+import static org.testfx.util.WaitForAsyncUtils.waitForAsyncFx;
 
 public class DatabaseSettingsPaneControllerTest extends ApplicationWithSetStageTest {
 
+    private static final DatabaseConnectionService databaseConnectionService;
     private static final DatabaseConnectionPreferences databaseConnectionPreferences;
 
     static {
@@ -21,6 +50,8 @@ public class DatabaseSettingsPaneControllerTest extends ApplicationWithSetStageT
         databaseConnectionPreferences.setUsername("abcd");
         databaseConnectionPreferences.setPassword("password".toCharArray());
         databaseConnectionPreferences.setDatabaseURL("jdbc:mysql://localhost:3306/client_schedule");
+
+        databaseConnectionService = new DatabaseConnectionService(databaseConnectionPreferences);
     }
 
     public DatabaseSettingsPaneControllerTest() {
@@ -81,12 +112,58 @@ public class DatabaseSettingsPaneControllerTest extends ApplicationWithSetStageT
         assertThat(databaseURLTextField.getText(), equalTo(String.format("jdbc:mysql://%s/%s", hostTextField.getText(), databaseNameTextField.getText())));
     }
 
+    @Test
+    public void clicking_testConnection_button_starts_databaseConnectionService() throws Exception {
+        clickOn("#testConnectionButton", MouseButton.PRIMARY);
+
+        var databaseConnectionServiceIsRunning = waitForAsyncFx(10L, databaseConnectionService::isRunning);
+
+        assertThat(databaseConnectionServiceIsRunning, is(true));
+    }
+
+    @Test
+    public void connection_test_progress_shows_when_connection_test_starts() throws Exception {
+        clickOn("#testConnectionButton", MouseButton.PRIMARY);
+
+        var connectionAttemptProgress = (Control) lookup("#connectionAttemptProgress").query();
+
+        Assertions.assertThat(connectionAttemptProgress).isVisible();
+    }
+
+    @Test
+    public void connection_error_label_shows_when_connection_test_fails() throws Exception {
+        doubleClickOn("#hostTextField", MouseButton.PRIMARY).write("127.0.0.1");
+        doubleClickOn("#databaseNameTextField", MouseButton.PRIMARY).write("client_schedule");
+        doubleClickOn("#userNameTextField", MouseButton.PRIMARY).write("admin");
+        doubleClickOn("#passwordTextField", MouseButton.PRIMARY).write("bad-password");
+        
+        clickOn("#testConnectionButton", MouseButton.PRIMARY);
+
+        var connectionErrorStatusLabel = (Labeled) sleep(3_000L).lookup("#connectionErrorStatusLabel").query();
+
+        Assertions.assertThat(connectionErrorStatusLabel).isVisible();
+    }
+    
+    @Test
+    public void connection_error_label_is_hidden_when_connection_test_fails() throws Exception {
+        doubleClickOn("#hostTextField", MouseButton.PRIMARY).write("127.0.0.1");
+        doubleClickOn("#databaseNameTextField", MouseButton.PRIMARY).write("client_schedule");
+        doubleClickOn("#userNameTextField", MouseButton.PRIMARY).write("admin");
+        doubleClickOn("#passwordTextField", MouseButton.PRIMARY).write("password");
+        
+        clickOn("#testConnectionButton", MouseButton.PRIMARY);
+
+        var connectionErrorStatusLabel = (Labeled) sleep(3_000L).lookup("#connectionErrorStatusLabel").query();
+
+        Assertions.assertThat(connectionErrorStatusLabel).matches(node -> !node.isVisible());
+    }
+
     private static Parent createRoot() {
         var fxmlPath = DisplayedView.DisplayedPane.DATABASE_SETTINGS_PANE.getFxmlPath();
         var controller = new DatabaseSettingsPaneController(
-                new DatabaseConnectionService(databaseConnectionPreferences)
-        );
+                databaseConnectionService);
 
         return (Parent) FxmlWithControllerLoader.load(fxmlPath, controller);
     }
+
 }
