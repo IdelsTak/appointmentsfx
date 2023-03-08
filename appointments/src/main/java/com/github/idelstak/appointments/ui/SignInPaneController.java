8875b5c071871864e41db5df20284b68248f83eb
Hiram K <hiram.kamau@outlook.com>
2023-03-08 17:53:11 +0300

    Update SignInPaneController.java

--- appointments/src/main/java/com/github/idelstak/appointments/ui/SignInPaneController.java
+++ appointments/src/main/java/com/github/idelstak/appointments/ui/SignInPaneController.java
@@ -1,12 +1,38 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 Hiram K
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.github.idelstak.appointments.ui;
 
 import com.github.idelstak.appointments.signin.Credentials;
+import com.github.idelstak.appointments.signin.SignInService;
+import com.github.idelstak.appointments.signin.SignInService.SignInStatus;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.ObjectProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -18,6 +44,7 @@ import javafx.scene.layout.VBox;
 
 public class SignInPaneController {
 
+    private static final Logger LOG = Logger.getLogger(SignInPaneController.class.getName());
     @FXML
     private ResourceBundle resources;
     @FXML
@@ -35,20 +62,31 @@ public class SignInPaneController {
     @FXML
     private TextField userNameTextField;
 
-    private final List<Credentials> allCredentials;
+    private final SignInService signInService;
 
-    public SignInPaneController(Collection<Credentials> credentials) {
-        allCredentials = new ArrayList<>(credentials);
+    public SignInPaneController(SignInService signInService) {
+        this.signInService = signInService;
     }
 
     @FXML
     protected void initialize() {
         signInErrorLabel.setVisible(false);
-        signInProgressIndicator.setVisible(false);
+        signInService
+                .getSignInStatusProperty()
+                .addListener((observable, oldSignInStatus, newSignInStatus) -> {
+                    signInErrorLabel.setVisible(newSignInStatus == SignInStatus.SIGNED_OUT);
+                });
+
+        Platform.runLater(() -> {
+            signInProgressIndicator.visibleProperty().bind(signInService.runningProperty());
+            signInProgressIndicator.progressProperty().bind(signInService.progressProperty());
+        });
     }
 
     @FXML
     protected void signIn(ActionEvent event) {
-        signInProgressIndicator.setVisible(true);
+        var passwordText = passwordField.getText();
+
+        signInService.check(new Credentials(userNameTextField.getText(), passwordText == null ? new char[]{} : passwordText.toCharArray()));
     }
 }
